4853b2e62b771f25e50864a4b2ebf6d83285b2e0
Ray <yamscript@gmail.com>
2025-10-15 23:08:41 +0300

    first

--- cfg/general.inc.php
+++ cfg/general.inc.php
@@ -3,8 +3,8 @@
 // db
 const DB_HOST = '127.0.0.1';
 const DB_USER = 'root';
-const DB_PASS = '';
-const DB_NAME = 'test';
+const DB_PASS = 'root';
+const DB_NAME = 'aeon';
 // common
 const SITE_SCHEME = 'http';
 const DEFAULT_TIMEZONE = '240';
